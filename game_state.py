e4e9b07b8c407fd46d43030aedadd9338b006ec3
eoincaughey <148149411+eoincaughey@users.noreply.github.com>
2025-10-14 22:44:12 +0200

    v1.0.0

--- game_state.py
+++ game_state.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 
 game_state = {
     "balance": 15000,
     "reputation": 0,
-    "noOfEmailAddresses": 0,
-=======
-
-game_state = {
-    "balance": 15000,
-    "reputation": 0,
-    "noOfEmailAddresses": 0,
->>>>>>> c3c416a88a1d162004d8e05cd506037914d6e02c
-}
+    "noOfEmailAddresses": 0,}
